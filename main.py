5fa024ebecc70ff22208e691f987c557fc71a483
Yakostoch <96203947+Yakostoch@users.noreply.github.com>
2023-06-02 23:51:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
 session = vk_api.VkApi(token='vk1.a.mvd-lcDrY37nLK0QA5hid_T3lfK3mmhIbEjSZ8QQKpwx8fLtLCTKT5JOJJYdRA4TTEiiBy1utPe5OgQmnKpFJ4e4PKgfH60Rs7VPW6kB9rk0V36sj3NwxLu8DQZUzSoHBWUxoviKbE93u4MlHecUtsuvXYDbM4hF5T_i8PclivariqZv5142nXElJ8Gd366sYSR1Kfj5d_LZ-8y9lmpNaQ')
-
+# САША ДАУн
 def send_message(user_id, message):
     session.method("messages.send",{ #Обращаемся к методу method  и к методу messages.send, передаем данные (user_id, message)
         "user_id": user_id,
